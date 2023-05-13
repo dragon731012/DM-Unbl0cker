e346cb890daed1702ab659e8d6451f34cc3d8ab2
Lekang Ji <74276537+22yeetz22@users.noreply.github.com>
2023-05-13 07:13:09 -0500

    Update script.js
    add butotnscript

--- static/settings/script.js
+++ static/settings/script.js
@@ -54,11 +54,56 @@ document.cookie="icon="+url+"; expires=Thu, 18 Dec 9013 12:00:00 UTC; path=/";
 setfavicon(url); 
 }
 function setfavicon(favicon){
-(function() {
+    (function() {
        var link=document.querySelector("link[rel*='icon']") || document.createElement('link');
        link.type='image/x-icon';
        link.rel='shortcut icon';
        link.href=favicon;
        document.getElementsByTagName('head')[0].appendChild(link);
    })();
+}
+
+function buttonClick() {
+    var imported=document.getElementById('importing').value;
+    var title2=document.getElementById('title').value;
+    var favicon=document.getElementById('url1').value;
+
+    if (imported==''){
+        document.title=title2;
+        document.cookie='title2='+title2+'; expires=Thu, 18 Dec 9013 12:00:00 UTC; path=/';
+
+        var t = document.querySelector('head');
+        var f = document.createElement('link');
+        f.setAttribute('rel','shortcut icon');
+        f.setAttribute('href',favicon);
+        t.appendChild(f);
+        document.cookie='favicon='+favicon+'; expires=Thu, 18 Dec 9013 12:00:00 UTC; path=/';
+
+        window.location.pathname='/';
+    }
+    else{
+        var url=importing.value;
+        var originalurl=url;
+
+        document.title=originalurl;
+        document.cookie='title2='+originalurl+'; expires=Thu, 18 Dec 9013 12:00:00 UTC; path=/';
+
+        if (url.includes('https://')==false){
+            url='https://'+url;
+        }
+
+        url='https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url='+url;
+
+        document.cookie='favicon='+url+'; expires=Thu, 18 Dec 9013 12:00:00 UTC; path=/';
+
+        var t = document.querySelector('head');
+        var f = document.createElement('link');
+        f.setAttribute('rel','shortcut icon');
+        f.setAttribute('href',url);
+        t.appendChild(f);
+        function waitf(){
+            window.location.pathname='/';
+        }
+        setTimeout(waitf,15);
+    }
 }
