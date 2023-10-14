a20cbfb2d1209c28d28c8e654024f5e8abd3eaab
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-10-14 10:00:40 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<p align="center"><img src="/static/img/logo.png" height="200">
+<p align="center"><img src="/static/img/logogif.gif" height="200">
 </p>
 
 <h1 align="center">DM unbl0cker</h1>
--- index.mjs
+++ index.mjs
@@ -17,5 +17,4 @@ server.on('upgrade', (req, socket, head) => {
 	if(bare.route_upgrade(req, socket, head))return;
 	socket.end();
 });
-
-server.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8080, () => {console.log("DM Unbl0cker running at http://localhost:8080")});
