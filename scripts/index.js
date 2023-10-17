cb1d12590c2d5625ec9067dad0a200fc1690faf5
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-10-16 22:00:29 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -22,15 +22,28 @@ form.addEventListener("submit", async (event) => {
     throw err;
   }
   const url = search(address.value, searchEngine.value);
-  var white = document.createElement('img');
+      var white = document.createElement('img');
             white.style.cursor="pointer";
             white.style.position = "absolute";
             white.style.width = "100%";
             white.style.height = "100%";
-            white.src = "/img/white.jpg";
+            white.style.zIndex="1";
+            white.src = "/img/black.jpeg";
             white.style.right = "0px";
             white.style.top = "0px";
             document.body.appendChild(white);
+
+            var loading = document.createElement('img');
+            loading.style.cursor="pointer";
+            loading.style.width = "125px";
+            loading.style.height = "125px";
+            loading.style.position="absolute";
+            loading.style.zIndex="2"; 
+            loading.src = "/img/loading.gif";
+            loading.style.top="50%";
+            loading.style.left="50%";
+            loading.style.transform="translate(-50%, -50%)";
+            document.body.appendChild(loading);
 
             var iframe = document.createElement('iframe');
 
@@ -38,9 +51,9 @@ form.addEventListener("submit", async (event) => {
             iframe.style.width = "100%";
             iframe.style.height = "100%";
             iframe.style.top = "0px";
-            iframe.style.zIndex="99999999";
             iframe.style.left = "0px";
             iframe.id = "iframe";
+            iframe.style.zIndex="9999999999999999";
             iframe.style.border = "none";
             iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
             document.body.appendChild(iframe);
@@ -77,4 +90,5 @@ form.addEventListener("submit", async (event) => {
             };
 
             document.body.appendChild(open);
+
 });
