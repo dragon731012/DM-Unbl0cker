0a691815a2990c236dd3edc0c48a67f93fe6e905
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-10-17 21:38:17 -0500

    Create load.js

---
+++ scripts/load.js
@@ -0,0 +1,25 @@
+document.getElementById("regform").addEventListener("submit", async (event) => {
+  event.preventDefault();
+  try {
+    await registerSW();
+  } catch (err) {
+    alert(err.toString());
+    throw err;
+  }
+  const url = search(address.value, searchEngine.value);
+
+            var iframe = document.createElement('iframe');
+
+            iframe.style.position = "absolute";
+            iframe.style.width = "100%";
+            iframe.style.height = "100%";
+            iframe.style.top = "0px";
+            iframe.style.left = "0px";
+            iframe.style.display="none";
+            iframe.id = "iframe";
+            iframe.style.zIndex="9999999999999999";
+            iframe.style.border = "none";
+            iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+            document.body.appendChild(iframe);
+
+});
