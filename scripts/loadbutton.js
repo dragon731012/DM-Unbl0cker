aac49febae311f444558123ce69304c04a0ef773
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-10-21 21:01:23 -0500

    Create loadbutton.js

---
+++ scripts/loadbutton.js
@@ -0,0 +1,29 @@
+async function loadsw(){
+  try {
+    await registerSW();
+  } catch (err) {
+    alert(err.toString());
+    throw err;
+  }
+  const url = search("", "");
+
+            var iframe = document.createElement('iframe');
+
+            iframe.style.position = "absolute";
+            iframe.style.width = "100%";
+            iframe.style.height = "100%";
+            iframe.style.top = "0px";
+            iframe.style.left = "0px";
+            iframe.style.display="none";
+            iframe.id = "iframe";
+            iframe.style.zIndex="9999999999999999";
+            iframe.style.border = "none";
+            iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+            document.body.appendChild(iframe);
+  function wait(){
+    window.location.reload(1);
+  }
+  setTimeout(wait,100);
+}
+
+document.getElementById("rb").addEventListener("click",loadsw);
