2bc6a6a4d724b9dc5a2aaabdb83e1b73d141af59
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-10-28 16:53:43 -0500

    Update cursor.js

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,6 +1,17 @@
 function get_cookie(cookie_name) { const value = "; " + document.cookie; const parts = value.split("; " + cookie_name + "="); if (parts.length === 2) return parts.pop().split(";").shift(); } 
 
+var script=document.createElement("script");
+script.src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
+document.body.appendChild(script);
+
 var isenabled=get_cookie("cursor");
+
+var iframepos = $("#frame").position();
+
+$('#frame').contents().find('html').on('mousemove', function (e) { 
+    var mousex = e.clientX + iframepos.left; 
+    var mousey = e.clientY + iframepos.top;
+})
 
 if (isenabled=="on"){
   var ring = document.createElement('img');
@@ -32,7 +43,7 @@ if (isenabled=="on"){
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)!=true){ 
     document.addEventListener('contextmenu', event => event.preventDefault());
     document.addEventListener("mouseleave", (event) => {  
-      if (event.clientY<0 || event.clientX<0 || (event.clientX>window.innerWidth || event.clientY>window.innerHeight)) {  
+      if (mousey<0 || mousex<0 || (mousex>window.innerWidth || mousey>window.innerHeight)) {  
         cursor1.style.display="none";
         ring.style.display="none";
       }  
@@ -40,13 +51,13 @@ if (isenabled=="on"){
     window.addEventListener('mousemove', (event) => {
       cursor1.style.display="block";
       ring.style.display="block";
-      var toleft=event.clientX-3;
-      var totop=event.clientY-5;
+      var toleft=mousex-3;
+      var totop=mousey-5;
       cursor1.style.left=toleft+"px";
       cursor1.style.top=totop+"px";
       function waiting(){
-        var toleft2=event.clientX-14.5;
-        var totop2=event.clientY-14;
+        var toleft2=mousex-14.5;
+        var totop2=mousey-14;
         ring.style.left=toleft2+"px";
         ring.style.top=totop2+"px";
       }
