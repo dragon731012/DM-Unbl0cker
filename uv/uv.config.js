910a5c0af2321d99bc27e9397a3bd611bd572576
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-10-28 09:07:47 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bareserverv2.onrender.com',
+    bare: 'https://bare-server-esc1.onrender.com',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
