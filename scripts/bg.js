da44ba92b2cc849364e3d6ae3470a64be61bc52c
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-10-29 07:27:35 -0500

    Update bg.js

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.particle { width: 0px; opacity: 1; border-radius: 50%; height: 0px; left: 50%; top: 50%; background-color: white; position: absolute; } #particlebox { color: transparent; z-index: -9999999999; background: transparent; position: absolute; left: 0px; top: 0px; overflow: hidden; width: 100%; height: 100%; } body { background-color: black; overflow:hidden; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+    
     setInterval(() => {
       var particle = document.createElement("div");
       particle.className = "particle";
