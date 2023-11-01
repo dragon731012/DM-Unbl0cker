9dfadd7ce30e4945e85cb3a27fb19a85a0a7ce79
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-11-01 18:19:55 -0500

    Update cookieclickerhack(WIP).js

--- games/gfiles/cookieclickerhack(WIP).js
+++ games/gfiles/cookieclickerhack(WIP).js
@@ -3,5 +3,5 @@ var doc = topdoc.document.getElementById("iframe").contentWindow;
 
 
 var script=topdoc.document.createElement("script");
-script.src="https://raw.githubusercontent.com/dragon731012/-WORKING-bookmarklets-and-games/main/other/cookieclickerh%40ack.js";
+script.src="https://raw.githubusercontent.com/dragon731012/dm-unbl0cker/static-current/games/gfiles/cookieclickerhacks/opensesame.js";
 doc.document.body.appendChild(script);
