a4c86465523c8083baa6ebc1b3c13dd12748a1dd
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-11-01 17:02:51 -0500

    Create cookieclickerhack(WIP).js

---
+++ games/gfiles/cookieclickerhack(WIP).js
@@ -0,0 +1,7 @@
+var topdoc=document.getElementById("frame").contentWindow; 
+var doc = topdoc.document.getElementById("iframe").contentWindow; 
+
+
+var script=topdoc.document.createElement("script");
+script.src="https://raw.githubusercontent.com/dragon731012/-WORKING-bookmarklets-and-games/main/other/cookieclickerh%40ack.js";
+doc.document.body.appendChild(script);
