a92ea19970f6227b30d99d5f44ec8983640210e0
dragonmaster73101 <105754685+dragon731012@users.noreply.github.com>
2023-11-01 20:43:29 -0500

    Update cookieclickerhack.js

--- games/gfiles/cookieclickerhack.js
+++ games/gfiles/cookieclickerhack.js
@@ -1,6 +1,11 @@
-function runfile(file){
-  var topdoc=document.getElementById("frame").contentWindow; 
-  var doc = topdoc.document.getElementById("iframe").contentWindow; 
+function runfile(file,top){
+  if (top){
+    var topdoc=document.getElementById("frame").contentWindow; 
+    var doc = topdoc.document.getElementById("iframe").contentWindow; 
+  }
+  else{
+    var doc = document.getElementById("iframe").contentWindow; 
+  }
   
   var script=topdoc.document.createElement("script");
   script.src=window.location.origin+"/games/gfiles/cookieclickerhacks/"+file;
