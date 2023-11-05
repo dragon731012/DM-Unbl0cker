b705331839b948aa43d8633df5f03ed3e46bfb12
Potato <105754685+dragon731012@users.noreply.github.com>
2023-11-05 11:54:50 -0600

    Update bg.js

--- scripts/bg.js
+++ scripts/bg.js
@@ -26,6 +26,14 @@ $(document).ready(function () {
         var hovercolor="#0075ba";
         var navcolor="#0066FF";
     }
+    else if (get_cookie("theme")=="fire and ice"){
+        var particlecolor="multi";
+        var backgroundcolor="linear-gradient(90deg, rgb(255, 0, 0) 0%, rgba(0,0,0,1) 51%, rgb(34, 181, 255) 100%);";
+        var hovercolor="#a6a6a6";
+        var navcolor="black";
+        var multicolor1="rgb(255, 123, 0)";
+        var multicolor2="rgb(35, 168, 235)";
+    }
     else if (get_cookie("theme")=="thanksgiving"){
         var particlecolor="#870020";
         var backgroundcolor="#856100";
@@ -83,48 +91,95 @@ $(document).ready(function () {
 
     var style = document.createElement('style');
     style.type = 'text/css';
-    style.innerHTML = "@import url('https://fonts.googleapis.com/css2?family=Ultra&display=swap');#chats { background-color:"+backgroundcolor+" !important; } #fsbutton{background-color:"+navcolor+" !important;} ul.navbar {background-color: "+navcolor+" !important;} li.nav-item2 a:hover,li.nav-item a:hover { background-color: "+hovercolor+" !important; } .black{ background-color:"+backgroundcolor+" !important; } .search{ background-color:"+backgroundcolor+" !important; } .particle { width: 0px; opacity: 1; border-radius: 50%; height: 0px; left: 50%; top: 50%; background-color: "+particlecolor+"; position: absolute; } #particlebox { color: transparent; z-index: -9999999999; background: transparent; position: absolute; left: 0px; top: 0px; overflow: hidden; width: 100%; height: 100%; } body { background-color: "+backgroundcolor+" !important; overflow:hidden;font-family:Ultra, serif !important;}";
+    style.innerHTML = "@import url('https://fonts.googleapis.com/css2?family=Ultra&display=swap');#chats { background:"+backgroundcolor+" !important; } #fsbutton{background-color:"+navcolor+" !important;} ul.navbar {background-color: "+navcolor+" !important;} li.nav-item2 a:hover,li.nav-item a:hover { background-color: "+hovercolor+" !important; } .black{ background:"+backgroundcolor+" !important; } .search{ background:"+backgroundcolor+" !important; } .particle { width: 0px; opacity: 1; border-radius: 50%; height: 0px; left: 50%; top: 50%; background-color: "+particlecolor+"; position: absolute; } #particlebox { color: transparent; z-index: -9999999999; background: transparent; position: absolute; left: 0px; top: 0px; overflow: hidden; width: 100%; height: 100%; } body { background: "+backgroundcolor+" !important; overflow:hidden;font-family:Ultra, serif !important;}";
     document.getElementsByTagName('head')[0].appendChild(style);
 
 
     if (window.location.pathname!="/" && window.location.pathname!="/chat"){
-        setInterval(() => {
-          var particle = document.createElement("div");
-          particle.className = "particle";
-          document.getElementById("particlebox").appendChild(particle);
-          var x = Math.floor(Math.random() * window.innerWidth);
-          var y = Math.floor(Math.random() * window.innerHeight);
-    
-          var speed = Math.floor(Math.random() * 4000);
-          var size = Math.floor(Math.random() * 25);
-    
-          if (Math.floor(Math.random() * 2) == 1) {
-            if (y > x) {
-              x = -50;
-            } else {
-              y = -50;
-            }
-          } else {
-            if (y > x) {
-              x = window.innerWidth + 50;
-            } else {
-              y = window.innerHeight + 50;
-            }
-          }
-          $(particle).animate(
-            {
-              left: x + "px",
-              top: y + "px",
-              opacity: "0.2",
-              display: "none",
-              height: 10 + size + "px",
-              width: 10 + size + "px"
-            },
-            4500 + speed,
-            function () {
-              particle.remove();
-            }
-          );
-        }, 100);
+        if (particlecolor=="multi"){
+            setInterval(() => {
+              var particle = document.createElement("div");
+              particle.className = "particle";
+              var rancolor=Math.floor(Math.random() * 2);
+              if (rancolor==1){
+                  particle.style.backgroundColor=multicolor1+"!important";
+              } else{
+                  particle.style.backgroundColor=multicolor2+"!important";
+              }
+              document.getElementById("particlebox").appendChild(particle);
+              var x = Math.floor(Math.random() * window.innerWidth);
+              var y = Math.floor(Math.random() * window.innerHeight);
+        
+              var speed = Math.floor(Math.random() * 4000);
+              var size = Math.floor(Math.random() * 25);
+        
+              if (Math.floor(Math.random() * 2) == 1) {
+                if (y > x) {
+                  x = -50;
+                } else {
+                  y = -50;
+                }
+              } else {
+                if (y > x) {
+                  x = window.innerWidth + 50;
+                } else {
+                  y = window.innerHeight + 50;
+                }
+              }
+              $(particle).animate(
+                {
+                  left: x + "px",
+                  top: y + "px",
+                  opacity: "0.2",
+                  display: "none",
+                  height: 10 + size + "px",
+                  width: 10 + size + "px"
+                },
+                4500 + speed,
+                function () {
+                  particle.remove();
+                }
+              );
+            }, 100);
+        } else{
+            setInterval(() => {
+              var particle = document.createElement("div");
+              particle.className = "particle";
+              document.getElementById("particlebox").appendChild(particle);
+              var x = Math.floor(Math.random() * window.innerWidth);
+              var y = Math.floor(Math.random() * window.innerHeight);
+        
+              var speed = Math.floor(Math.random() * 4000);
+              var size = Math.floor(Math.random() * 25);
+        
+              if (Math.floor(Math.random() * 2) == 1) {
+                if (y > x) {
+                  x = -50;
+                } else {
+                  y = -50;
+                }
+              } else {
+                if (y > x) {
+                  x = window.innerWidth + 50;
+                } else {
+                  y = window.innerHeight + 50;
+                }
+              }
+              $(particle).animate(
+                {
+                  left: x + "px",
+                  top: y + "px",
+                  opacity: "0.2",
+                  display: "none",
+                  height: 10 + size + "px",
+                  width: 10 + size + "px"
+                },
+                4500 + speed,
+                function () {
+                  particle.remove();
+                }
+              );
+            }, 100);
+        }
     }
   });
