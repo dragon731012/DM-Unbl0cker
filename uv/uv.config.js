12c6bdf6aa89ced2d1cb4837f9d4ef55fa95af7d
Potato <105754685+dragon731012@users.noreply.github.com>
2023-11-09 08:31:44 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare-server-3y9o.onrender.com',
+    bare: 'https://bare-server-75ny.onrender.com',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
