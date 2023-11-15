98493984efa09bd35b2a2b6ff1eca4078d2f0338
Potato <105754685+dragon731012@users.noreply.github.com>
2023-11-15 13:31:44 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare-server-lg7h.onrender.com',
+    bare: 'https://login.computers-science.online/outerspace',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
