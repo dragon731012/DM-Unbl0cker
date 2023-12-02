a8c80df404e9f1bc8a2cf4c3470dd44865bb5696
Potato <105754685+dragon731012@users.noreply.github.com>
2023-12-02 16:30:50 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://uv.thechefmethod.com/bare1/',
+    bare: 'https://pyth0n.software/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
