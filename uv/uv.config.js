bcb5332a09d874dc8d38f9f456ddd2cfd9e09c15
Potato <105754685+dragon731012@users.noreply.github.com>
2023-12-06 14:01:13 +0000

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://pyth0n.software/bare/',
+    bare: 'https://bare.tiwhosts.me',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
