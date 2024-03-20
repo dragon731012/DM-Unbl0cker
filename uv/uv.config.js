fe4acbf8d2ab058fbf3ea64323e903f8c6299e57
Potato <105754685+dragon731012@users.noreply.github.com>
2024-03-20 18:42:20 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.benrogo.net',
+    bare: 'https://dm-unbl0cker-bare-server-iota.vercel.app',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
